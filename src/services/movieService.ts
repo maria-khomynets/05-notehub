fa212065f6541b36e2e6aa241079ca8ab1c9d0d3
Maria Khomynets <masha.khomynets@gmail.com>
2026-07-01 11:47:00 +0300

    Initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+interface MoviesHttpResponse {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+}
+
+const token = `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`;
+
+export async function fetchMovies(
+  search: string,
+  currentPage: number = 1,
+): Promise<MoviesHttpResponse> {
+  const parameters = {
+    params: {
+      query: search,
+      page: currentPage,
+    },
+    headers: {
+      Authorization: token,
+      accept: "application/json",
+    },
+  };
+  const response = await axios.get<MoviesHttpResponse>(
+    "https://api.themoviedb.org/3/search/movie",
+    parameters,
+  );
+
+  return response.data;
+}
